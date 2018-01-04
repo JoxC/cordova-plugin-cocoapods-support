82e2d61c43fbf32ef41fe692b641434d7c0d40f4
Chen Xiaohua <chen_xiaohua@foxmail.com>
2018-01-04 15:50:55 +0800

    add plug info output for npm install before plug install

--- scripts/npmInstall.js
+++ scripts/npmInstall.js
@@ -1,7 +1,7 @@
 var exec = require('child_process').execSync;
 
 module.exports = function(context) {
-
+    console.log('plug info: ' + JSON.stringify(context.opts.plugin.pluginInfo));
     exec('npm install', {
         cwd: context.opts.plugin.pluginInfo.dir
     });
